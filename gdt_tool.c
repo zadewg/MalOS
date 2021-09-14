967468f8551bd8b8c56b5788533f41bb5f1f7ca6
zadewg <zadewg@gmail.com>
2021-09-14 20:28:45 +0200

    Fix format error in GDT Tool

--- gdt_tool.c
+++ gdt_tool.c
@@ -46,28 +46,33 @@
 	             SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
 	             SEG_PRIV(3)     | SEG_DATA_RDWR
 
-void create_descriptor(uint32_t base, uint32_t limit, uint16_t flag)                                                                                        {                                                                                                                                                           
-	uint64_t descriptor;
-	
-	// Create the high 32 bit segment														
-	descriptor  =  limit       & 0x000F0000;         // set limit bits 19:16
-	descriptor |= (flag <<  8) & 0x00F0FF00;         // set type, p, dpl, s, g, d/b, l and avl fields
-	descriptor |= (base >> 16) & 0x000000FF;         // set base bits 23:16
-	descriptor |=  base        & 0xFF000000;         // set base bits 31:24
-	
-	// Shift by 32 to allow for low part of segment
-	descriptor <<= 32;                                                                                                                                          
-	// Create the low 32 bit segment
-	descriptor |= base  << 16;                       // set base bits 15:0
-	descriptor |= limit  & 0x0000FFFF;               // set limit bits 15:0
-	printf("0x%.16llX\n", descriptor);
-}                                                                                                                                                                                                                                                                                                                       int main(void)
+void create_descriptor(uint32_t base, uint32_t limit, uint16_t flag)
 {
-	create_descriptor(0, 0, 0);			// required null entry
-	create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL0));
-	create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL0));
-	create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL3));
-	create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL3));
-
-	return 0;                                                                                                                                          
+    uint64_t descriptor;
+ 
+    // Create the high 32 bit segment
+    descriptor  =  limit       & 0x000F0000;         // set limit bits 19:16
+    descriptor |= (flag <<  8) & 0x00F0FF00;         // set type, p, dpl, s, g, d/b, l and avl fields
+    descriptor |= (base >> 16) & 0x000000FF;         // set base bits 23:16
+    descriptor |=  base        & 0xFF000000;         // set base bits 31:24
+ 
+    // Shift by 32 to allow for low part of segment
+    descriptor <<= 32;
+ 
+    // Create the low 32 bit segment
+    descriptor |= base  << 16;                       // set base bits 15:0
+    descriptor |= limit  & 0x0000FFFF;               // set limit bits 15:0
+ 
+    printf("0x%.16llX\n", descriptor);
+}
+ 
+int main(void)
+{
+    create_descriptor(0, 0, 0);
+    create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL0));
+    create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL0));
+    create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL3));
+    create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL3));
+ 
+    return 0;
 }
